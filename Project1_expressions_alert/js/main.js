6cbf00ff867ee15fddc8f7ce283a849c2302fee4
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-11 22:16:44 +0100

    Add files via upload

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,13 @@
+
+window.alert("welcome!")//Pop up window with welcome wording//
+var B="THIS "//Define the variable B//
+var C="IS"//Define the variable C//
+document.write(B+C)// concatenate B+C//
+document.write(" JS practice")//Write JS practice//
+var blues="Hello"//Define variable blues//
+var blues=blues.fontcolor("blue")//assign variable to be blue//
+document.write(blues)//Write Hello in blue//
+
+
+var A="3"//An expression code to define the variable A//
+document.write(A)//wWrite variable A//
