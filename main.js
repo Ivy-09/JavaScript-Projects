d57e6c1fe16aca9b1952a749787c37b4c8f4ff07
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-11 19:16:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
+window.alert("welcome!")//Pop up window with welcome wording//
+var B="THIS "//Define the variable B//
+var C="IS"//Define the variable C//
+document.write(B+C)// concatenate B+C//
+document.write(" JS practice")//Write JS practice//
+var blues="Hello"//Define variable blues//
+var blues=blues.fontcolor("blue")//assign variable to be blue//
+document.write(blues)//Write Hello in blue//
 
-window.alert("welcome!")
-var B="THIS "
-var C="IS"
-document.write(B+C)
-document.write(" JS practice")
-var blues="Hello"
-var blues=blues.fontcolor("blue")
-document.write(blues)
 
-var food="straberry",color="red"
-document.write(food)
-var A="3"
-document.write(A)
+var A="3"//An expression code to define the variable A//
+document.write(A)//wWrite variable A//
