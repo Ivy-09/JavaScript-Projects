23a319a3dc1605cb837909e7f47976f5d2472dd6
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-11 19:15:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+window.alert("welcome!")
+var B="THIS "
+var C="IS"
+document.write(B+C)
+document.write(" JS practice")
+var blues="Hello"
+var blues=blues.fontcolor("blue")
+document.write(blues)
+
+var food="straberry",color="red"
+document.write(food)
+var A="3"
+document.write(A)
