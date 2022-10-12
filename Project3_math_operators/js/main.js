6abcaad3f72b32dd64147c0a4a0187224c93cebb
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-12 21:03:53 +0100

    Add files via upload

--- Project3_math_operators/js/main.js
+++ Project3_math_operators/js/main.js
@@ -1,10 +1,38 @@
 
-    function subtraction_Function(){
-      var Subtraction=5-2;
-      document.getElementById("Math").innerHTML="5-2= "+Subtraction;
-    }
-    
-    
+function addition_Function(){
+  var addition=1 + 4;
+  document.getElementById("addition").innerHTML="1 + 4= "+addition
+}
 
+    function subtraction_Function() {
+      var subtraction= 5 - 3;
+   
+    document.getElementById("Math").innerHTML="5 - 3 = "+subtraction;
+  }
+   function multiplication(){
+    var multiplication=20 * 10;
+    document.getElementById("multiplication").innerHTML="20 * 10="+multiplication;
+   }
+
+   function division(){
+    var division=20 / 10;
+    document.getElementById("division").innerHTML="20 / 10="+division;
+   }
+
+   function modulus_Operator(){
+    var modulus=25 % 7;
+    document.getElementById("modulus").innerHTML="25 % 7="+modulus;
+   }
+ 
+    var x=3;
+    x++;
+    document.write(x)
+
+    var y=3;
+    y--;
+    document.write(y)
+
+   window.alert(Math.random()) 
+   
 
 
