3548cc8f7ffc9472170a8c9e7dcf355fb801ea5f
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-12 21:05:48 +0100

    Update main.js

--- Project3_math_operators/js/main.js
+++ Project3_math_operators/js/main.js
@@ -1,38 +1,39 @@
-
 function addition_Function(){
   var addition=1 + 4;
   document.getElementById("addition").innerHTML="1 + 4= "+addition
-}
+}// addition function//
 
     function subtraction_Function() {
       var subtraction= 5 - 3;
    
     document.getElementById("Math").innerHTML="5 - 3 = "+subtraction;
-  }
+  } // subtraction function//
    function multiplication(){
     var multiplication=20 * 10;
     document.getElementById("multiplication").innerHTML="20 * 10="+multiplication;
    }
+   // multiply function//
 
    function division(){
     var division=20 / 10;
     document.getElementById("division").innerHTML="20 / 10="+division;
    }
-
+ // divide function//
    function modulus_Operator(){
     var modulus=25 % 7;
     document.getElementById("modulus").innerHTML="25 % 7="+modulus;
    }
- 
+ // modulus function//
     var x=3;
     x++;
     document.write(x)
-
+// ++ function//
     var y=3;
     y--;
     document.write(y)
 
    window.alert(Math.random()) 
+   // -- function//
    
 
 
