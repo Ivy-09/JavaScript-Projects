e6cb1e3544260dbfea9224e5263b7bf23d98a619
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-12 10:47:23 +0100

    Add files via upload

---
+++ Project2_functions/JS/JS.js
@@ -0,0 +1,11 @@
+
+var A="3"
+document.write(A)
+
+ function myFunction(){
+    var sentence="I am learning";
+    sentence +="JavaScript now!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+ }
+ 
+ 
