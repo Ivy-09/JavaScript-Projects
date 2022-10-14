6c43dbab1198f4aec6cb20cb00981c49645de539
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-14 14:31:07 +0100

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,15 @@
+document.write(typeof "word")//typeof operator for string//
+document.write(typeof 2)//typeof operator for number//
+document.write(2E310)//infinity//
+
+document.write("20"+30)//An expression combining a string and a number//
+document.write(20==3)//==operator//
+x=20;
+y="30";
+document.write(x===y);//===operator//
+document.write(2>4 && 3>2);//&&operator//
+document.write(10>2 || 8>2)//||operator//
+function not_Function (){
+    document.getElementById("Not").innerHTML =!(2 > 3);
+}// ! operator//
+
