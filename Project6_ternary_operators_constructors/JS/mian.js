c2931524e754d1ed9aee0ca9db480f3fcab038c3
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2022-10-19 14:17:53 +0100

    Update mian.js

--- Project6_ternary_operators_constructors/JS/mian.js
+++ Project6_ternary_operators_constructors/JS/mian.js
@@ -1,12 +1,34 @@
 function Age_Function() {
     Age=document.getElementById("Age").value;
     if (Age>=18){
-        Vote="You can vote!"
+        Vote="You can vote!";
     }
     else{
         vote="You can't vote";
     }
     document.getElementById("How_old_are_you?").innerHTML=vote;
 }
+//ternary operation//
+
+function food (Price,Calorie,Popularity){
+    this.food_Price=Price;
+    this.food_Calorie=Calorie;
+    this.food_Popularity=Popularity;
+}
+var eggSalad=new food("5","150","popular")
+function myFunction(){
+    document.getElementById("keywords_and_constructors").innerHTML=eggSalad.food_Price;
+}
+//this and new keywords//
+
    
-  
+  function count_Function{
+    document.getElementById("Counting").innerHTML=Count();
+    function Count(){
+        Var Strating_Point=9;
+        function Plus_one{Starting_point +=1;}
+        Plus_one();
+        return Starting_Point;
+    }
+  }
+  //this is  a nested function//
